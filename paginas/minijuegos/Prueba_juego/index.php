0ac48f96ec985660795018b648665a59fdee729f
oloralibro <freespamgratis@gmail.com>
2019-12-14 16:52:36 +0100

    Integrat el mapa a 4k i afegit el joc de recepció

---
+++ paginas/minijuegos/Prueba_juego/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="estilos.css">
+    <title>Proyecto - Prueba juego</title>
+    <script src="comportamiento.js"></script>
+</head>
+<body>
+    <h1 id="titulo"> Conecta los cables con su pareja. </h1>
+    <div class="left">
+        <div class="cableL" id="cl1" onclick="selected('cl1')"></div>
+
+        <div class="cableL" id="cl2" onclick="selected('cl2')"></div>
+
+        <div class="cableL" id="cl3" onclick="selected('cl3')"></div>
+
+        <div class="cableL" id="cl4" onclick="selected('cl4')"></div>
+    </div>
+
+    <div class="center">
+            
+    </div>
+
+    <div class="right">
+        <div class="cableR" id="cr1" onclick="comprobar('cr1')"></div>
+
+        <div class="cableR" id="cr2" onclick="comprobar('cr2')"></div>
+
+        <div class="cableR" id="cr3" onclick="comprobar('cr3')"></div>
+
+        <div class="cableR" id="cr4"onclick="comprobar('cr4')"></div>
+    </div>
+    <h2 id="info"></h1>
+    
+    <div id="pop-up">
+        <h1 id="mensajes">
+            ¡Felicidades, has contectado todos los cables!
+        </h1>
+        <div class="container" style="width: 100%; height: 30%; margin: 0 auto; text-align: center; margin-top: 15%">
+            <button id="replay" class="btn-popup">Volver a jugar</button>
+            <button id="menu" class="btn-popup">Volver al menú</button>
+        </div>
+    </div>   
+</body>
+</html>
---
+++ paginas/minijuegos/Prueba_juego/js/prba.js
@@ -0,0 +1,10 @@
+//Fichero para realizar pruebas------------------------------------------
+
+var cablesLeft = ['cl1', 'cl2', 'cl3', 'cl4'];
+var cablesRight = ['cr1', 'cr2', 'cr3', 'cr4'];
+var contador = 0;
+
+while (contador < 4){
+    var rnd = Math.floor(Math.random() * 4); //Devuelve un valor random entre 0 y 3
+
+}
