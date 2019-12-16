556a90a43390ab7313acd7cb260bdb39977f8f8f
oloralibro <freespamgratis@gmail.com>
2019-12-16 05:21:08 +0100

    Funcionalitat basica en presentació implementada

--- paginas/presentacion/js/presentacion.js
+++ paginas/presentacion/js/presentacion.js
@@ -1,4 +1,5 @@
-var info =
+//#region codi handmade
+/*var info =
   '{ "Diapositivas" : [' +
   '{ "name":"Emisió" , "image":"emisio.png", "textos":[{"parrafada":""},{"parrafada":""},{"parrafada":""}] },' +
   '{ "name":"Emisió" , "image":"transmisio.png", "textos":[{"parrafada":""},{"parrafada":""},{"parrafada":""}] },' +
@@ -59,4 +60,39 @@ function talk(text) {
 function eliminarText() {
   //Eliminem el fill creat
   interlocutor.removeChild(interlocutor.firstChild());
+}
+*/
+//#endregion
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
 }
