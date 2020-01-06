0ed7ec2d714a1fea43551f6990042bb413c7aae8
oloralibro <freespamgratis@gmail.com>
2020-01-06 12:17:01 +0100

    Reestructuració del projecte a php

---
+++ plantilla_base.php
@@ -0,0 +1,26 @@
+<?php require_once './resources/ti.php' ?>
+<html>
+
+<head>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <?php startblock('capcelera') ?>
+    <?php endblock() ?>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <h5 class="my-0 mr-md-auto font-weight-normal">Museu MNACTEC</h5>
+        <!--<input type="image" src="./img/espana.png" />
+        <input type="image" src="./img/english.png" onclick="location.href='./paginas/index.en.html'" />
+        <input type="image" src="./img/catalan.png" onclick="location.href='./paginas/index.cat.html'" /> -->
+
+        <a class="btn btn-primary" href="./paginas/about/aboutus.html">Qui Som?</a>
+
+    </nav>
+    <?php startblock('principal') ?>
+    <?php endblock() ?>
+</body>
+
+</html>
