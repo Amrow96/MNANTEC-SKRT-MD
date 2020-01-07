84edf765d6c8cca9404f171c0e9e2d7d5ce633d4
oloralibro <freespamgratis@gmail.com>
2020-01-07 13:23:21 +0100

    Solventats problemes minims de responsivitat

--- index.php
+++ index.php
@@ -5,55 +5,56 @@
 <?php endblock() ?>
 <?php startblock('principal') ?>
 
-<div class="container mt-5">
+<div class="container mt-5 col-10">
     <div class="card-deck mb-3 text-center">
-        <div class="card mb-4 box-shadow">
+        <!-- Mapa de la Radio -->
+        <div class="card mb-4">
             <div class="card-header">
                 <img class="card-img-top" src="./paginas/Prueba_map(4k)/recursos/miniatura.png" alt="Card image" style="width:100%">
             </div>
             <div class="card-body">
                 <!-- TODO: carregar els textos desde variables per fer-ho multilingue -->
-                <h2 class="my-0 font-weight-normal">Mapa de la Radio</h2>
+                <h2 class="titol">Mapa de la Radio</h2>
                 <ul class="list-unstyled mt-3 mb-4">
-                    <li>Puedes escuchar </li>
-                    <li>algunas de las emisoras </li>
-                    <li>de todo el mundo!</li>
+                    <li class="contingut">Puedes escuchar </li>
+                    <li class="contingut">algunas de las emisoras </li>
+                    <li class="contingut">de todo el mundo!</li>
                 </ul>
                 <button type="button" class="btn btn-lg btn-block btn-primary" onclick="location.href='./paginas/Prueba_map(4k)/index.html'">Ir al mapa!</button>
             </div>
         </div>
-        <div class="card mb-4 box-shadow">
+        <!-- Minijocs -->
+        <div class="card mb-4">
             <div class="card-header">
                 <img class="card-img-top" src="./img/minigames.png" alt="Card image" style="width:100%">
 
             </div>
             <div class="card-body">
-                <h2 class="my-0 font-weight-normal">Minijuegos</h2>
+                <h2 class="titol">Minijuegos</h2>
                 <ul class="list-unstyled mt-3 mb-4">
-                    <li>Emisión</li>
-                    <li>Transmisión</li>
-                    <li>Recepción</li>
+                    <li class="contingut">Emisión</li>
+                    <li class="contingut">Transmisión</li>
+                    <li class="contingut">Recepción</li>
                 </ul>
                 <button type="button" class="btn btn-lg btn-block btn-primary" onclick="location.href='./paginas/minijuegos/minijuegos.html'">A jugar!</button>
             </div>
         </div>
-        <div class="card mb-4 box-shadow">
+        <!-- Presentació -->
+        <div class="card mb-4">
             <div class="card-header">
                 <img class="card-img-top" src="./img/radio.png" alt="Card image" style="width:100%">
             </div>
             <div class="card-body">
-                <h2 class="my-0 font-weight-normal">Información</h2>
+                <h2 class="titol">Información</h2>
                 <ul class="list-unstyled mt-3 mb-4">
-                    <li>30 users included</li>
-                    <li>15 GB of storage</li>
-                    <li>Help center access</li>
+                    <li class="contingut">30 users included</li>
+                    <li class="contingut">15 GB of storage</li>
+                    <li class="contingut">Help center access</li>
                 </ul>
                 <button type="button" class="btn btn-lg btn-block btn-primary" onclick="location.href='./paginas/presentacion/presentacion.html'">Aprende!</button>
             </div>
         </div>
     </div>
-
-
 </div>
 
 <?php endblock() ?>
--- plantilla_base.php
+++ plantilla_base.php
@@ -5,19 +5,23 @@
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
     <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="./css/micss.css" rel="stylesheet">
     <?php startblock('capcelera') ?>
     <?php endblock() ?>
 </head>
 
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <h5 class="my-0 mr-md-auto font-weight-normal">Museu MNACTEC</h5>
-        <!--<input type="image" src="./img/espana.png" />
-        <input type="image" src="./img/english.png" onclick="location.href='./paginas/index.en.html'" />
-        <input type="image" src="./img/catalan.png" onclick="location.href='./paginas/index.cat.html'" /> -->
-
-        <a class="btn btn-primary" href="./paginas/about/aboutus.html">Qui Som?</a>
-
+        <h5 class="mr-auto ml-3 titol">Museu MNACTEC</h5>
+        <!-- TODO posar els inputs de tipus botó i fer que el fondo sigui la imatge
+        <div class="text-center">
+            <input type="image" src="./img/espana.png" />
+            <input type="image" src="./img/english.png" onclick="location.href='./paginas/index.en.html'" />
+            <input type="image" src="./img/catalan.png" onclick="location.href='./paginas/index.cat.html'" />
+        </div>-->
+        <div class="mt-3 mb-3">
+            <a class="btn btn-primary mr-3 titol right" href="./paginas/about/aboutus.html">Qui Som?</a>
+        </div>
     </nav>
     <?php startblock('principal') ?>
     <?php endblock() ?>
